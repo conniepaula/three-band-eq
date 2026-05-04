dce9bdc1cf7bf9cea1bd33d490b829795aafc9c6
Connie <conniedpaula@gmail.com>
2026-05-04 13:31:00 -0300

    include parameter attributes

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -3,6 +3,21 @@
 
 
 namespace param{
+
+juce::String unitToString(Unit unit)
+{
+    switch (unit) {
+        case Unit::decibels:
+            return "dB";
+        case Unit::hertz:
+            return "Hz";
+        case Unit::dbOct:
+            return "dB/Oct";
+        default:
+            return "";
+    }
+}
+
 juce::String pidToLabel(PID pID)
 {
     switch (pID)
@@ -61,28 +76,197 @@ juce::String pidToID(PID pID)
     return {};
 }
 
-void createParameter(UniqueRAPVector& paramsVector, PID pID, const RangeF& range, float defaultValue, const juce::String& unit, const int version)
+namespace range
 {
-//    const auto valueToString = [](float value, int maxLen)
-//    {
-//        return juce::String(value, maxLen);
-//    };
-//    
-//    const auto stringToValue = [](const juce::String& text)
-//    {
-//        return text.getFloatValue();
-//    };
-//
+RangeF biased(float start, float end, float bias) noexcept
+{
+    // https://www.desmos.com/calculator/ps8q8gftcr
+    const auto a = bias * .5f + .5f;
+    const auto a2 = 2.f * a;
+    const auto aM = 1.f - a;
+    
+    const auto r = end - start;
+    const auto aR = r * a;
+    if (bias != 0.f)
+        return
+    {
+        start, end,
+        [a2, aM, aR](float min, float, float x)
+        {
+            const auto denom = aM - x + a2 * x;
+            if (denom == 0.f)
+                return min;
+            return min + aR * x / denom;
+        },
+        [a2, aM, aR](float min, float, float x)
+        {
+            const auto denom = a2 * min + aR - a2 * x - min + x;
+            if (denom == 0.f)
+                return 0.f;
+            auto val = aM * (x - min) / denom;
+            return val > 1.f ? 1.f : val;
+        },
+        [](float min, float max, float x)
+        {
+            return x < min ? min : x > max ? max : x;
+        }
+    };
+    else return { start, end };
+}
+
+RangeF stepped(float start, float end, float steps) noexcept
+{
+    return { start, end, steps };
+}
+
+RangeF toggle() noexcept
+{
+    return stepped(0.f, 1.f);
+}
+
+RangeF lin(float start, float end) noexcept
+{
+    const auto range = end - start;
+    
+    return
+    {
+        start,
+        end,
+        [range](float min, float, float normalized)
+        {
+            return min + normalized * range;
+        },
+        [inv = 1.f / range](float min, float, float denormalized)
+        {
+            return (denormalized - min) * inv;
+        },
+        [](float min, float max, float x)
+        {
+            return juce::jlimit(min, max, x);
+        }
+    };
+}
+
+RangeF withCentre(float start, float end, float centre) noexcept
+{
+    const auto r = end - start;
+    const auto v = (centre - start) / r;
+    
+    return biased(start, end, 2.f * v - 1.f);
+}
+}
+
+namespace valueToString
+{
+ValueToString db()
+{
+    return [](float value, int)
+    {
+        return juce::String(value, 2) + "dB";
+    };
+}
+ValueToString hz()
+{
+    return [](float value, int maxLen)
+    {
+        if (value < 100.0f)
+            return juce::String(value, 2) + "Hz";
+        else if (value < 1000.0f)
+            return juce::String(value, 1) + "Hz";
+        else
+        {
+            auto k = value / 1000.0f;
+            return juce::String(k, 1) + "kHz";
+        }
+    };
+}
+ValueToString raw()
+{
+    return [](float value, int)
+    {
+        return juce::String(value, 2);
+    };
+}
+}
+
+namespace stringToValue
+{
+StringToValue db()
+{
+    return [](const juce::String& str)
+    {
+        return str.removeCharacters(unitToString(Unit::decibels)).getFloatValue();
+    };
+}
+StringToValue hz()
+{
+    return [](const juce::String& str)
+    {
+        auto s = str.removeCharacters(unitToString(Unit::hertz));
+        if (s.endsWith("k"))
+        {
+            s = s.dropLastCharacters(1);
+            return s.getFloatValue() * 1000.0f;
+        }
+        return s.getFloatValue();
+    };
+}
+StringToValue raw()
+{
+    return [](const juce::String& str)
+    {
+        return str.getFloatValue();
+    };
+}
+}
+
+void createParameter(UniqueRAPVector& paramsVector, PID pID, const RangeF& range, float defaultValue, const Unit& unit, const int version)
+{
+
+    ValueToString valToStr;
+    StringToValue strToVal;
+    
     const auto id = pidToID(pID);
     const juce::ParameterID jucePID {id, version};
     const auto label = pidToLabel(pID);
+    
+    switch (unit)
+    {
+        case Unit::decibels:
+        {
+            valToStr = valueToString::db();
+            strToVal = stringToValue::db();
+            break;
+        }
+        case Unit::hertz:
+        {
+            valToStr = valueToString::hz();
+            strToVal = stringToValue::hz();
+            break;
+        }
+        case Unit::unitless:
+        {
+            valToStr = valueToString::raw();
+            strToVal = stringToValue::raw();
+            break;
+        }
+        default:
+        {
+            jassertfalse;
+            break;
+        }
+    }
+    
+    
+    auto attributes = APFAttributes().withStringFromValueFunction(valToStr).withValueFromStringFunction(strToVal);
     
     
     DBG("Creating parameter: " << label << " | ID: " << id);
     jassert(id.isNotEmpty());
     
     
-    paramsVector.push_back(std::make_unique<APF>(jucePID, label, range, defaultValue, unit));
+    paramsVector.push_back(std::make_unique<APF>(jucePID, label, range, defaultValue, attributes));
+
 }
 
 void createChoiceParameter(UniqueRAPVector& paramsVector, PID pID, int version, const juce::StringArray& choiceArray, int defaultIndex)
@@ -94,6 +278,7 @@ void createChoiceParameter(UniqueRAPVector& paramsVector, PID pID, int version, 
     DBG("Creating parameter: " << label << " | ID: " << id);
     jassert(id.isNotEmpty());
     
+    
     paramsVector.push_back(std::make_unique<APC>(jucePID, label, choiceArray, defaultIndex));
 }
 
@@ -103,34 +288,36 @@ Layout createParameterLayout()
     const int version = 1;
     juce::StringArray slopes { "12 dB/Oct", "24 dB/Oct", "36 dB/Oct", "48 dB/Oct" };
     
-    //    createParameter(params, PID::highCutFreq, RangeF(0.1f, 10.0f, 0.05f,  0.25f), 1.0f, 20000, "Hz");
     createParameter(params, PID::lowCutFreq,
-                    RangeF(20.0f, 20000.0f, 1.0f,  0.25f),
+                    range::withCentre(20.0f, 20000.0f, 1000.0f),
                     20.0f,
-                    "Hz", version);
+                    Unit::hertz, version);
     
     createChoiceParameter(params, PID::lowCutSlope, version, slopes);
     
-
+    
     createParameter(params, PID::peakFreq,
-                    RangeF(20.0f, 20000.0f, 1.0f,  0.25f),
+                    range::withCentre(20.0f, 20000.0f, 1000.0f),
                     750.0f,
-                    "Hz", version);
-
+                    Unit::hertz, version);
+    
     // TODO: Improve skew factor
     createParameter(params, PID::peakGain,
                     RangeF(-24.0f, 24.0f, 0.5f,  0.6f),
                     0.0f,
-                    "dB", version);
-
+                    Unit::decibels, version);
+    
     // TODO: Improve skew factor
     createParameter(params, PID::peakQuality,
                     RangeF(0.1f, 10.0f, 0.05f,  0.4f),
                     1.0f,
-                    "", version);
+                    Unit::unitless, version);
     
-
-    createParameter(params, PID::highCutFreq, RangeF(20.0f, 20000.0f, 1.0f,  0.25f), 20000.0f, "Hz", version);
+    
+    createParameter(params, PID::highCutFreq,
+                    range::withCentre(20.0f, 20000.0f, 1000.0f),
+                    20000.0f,
+                    Unit::hertz, version);
     
     
     createChoiceParameter(params, PID::highCutSlope, version, slopes);
@@ -144,7 +331,7 @@ float getParamValue(std::vector<param::RAP*>& params, PID pID)
     auto* param = params[index];
     const auto norm = param->getValue();
     return param->getNormalisableRange().convertFrom0to1(norm);
-
+    
 }
 
 }
--- Source/Parameters.h
+++ Source/Parameters.h
@@ -20,8 +20,35 @@ using UniqueRAP = std::unique_ptr<RAP>;
 using UniqueRAPVector = std::vector<UniqueRAP>;
 using APP = juce::AudioProcessorParameter;
 using APF = juce::AudioParameterFloat;
+using APFAttributes = juce::AudioParameterFloatAttributes;
 using APC = juce::AudioParameterChoice;
 using RangeF = juce::NormalisableRange<float>;
+
+namespace range
+{
+    RangeF biased(float start, float end, float bias) noexcept;
+    RangeF stepped(float start, float end, float steps = 1.f) noexcept;
+    RangeF toggle() noexcept;
+    RangeF lin(float start, float end) noexcept;
+    RangeF withCentre(float start, float end, float centre) noexcept;
+}
+
+using ValueToString = std::function<juce::String(float, int)>;
+using StringToValue = std::function<float(const juce::String&)>;
+
+namespace valueToString
+{
+ValueToString db();
+ValueToString hz();
+ValueToString raw();
+}
+
+namespace stringToValue
+{
+StringToValue db();
+StringToValue hz();
+StringToValue raw();
+}
 
 enum class PID {
     lowCutFreq,
@@ -33,6 +60,16 @@ enum class PID {
     highCutSlope,
     numParams
 };
+
+enum class Unit {
+    decibels,
+    hertz,
+    unitless,
+    dbOct,
+    numUnits
+};
+
+juce::String unitToString(Unit unit);
 
 juce::String pidToLabel(PID pID);
 
